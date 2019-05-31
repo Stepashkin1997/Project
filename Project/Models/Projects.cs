41c7c2e5e2058db21220a918521c32020ae113a3
Никита Степашкин <33stepashkin33@gmail.com>
2019-05-31 21:35:44 +0700

    add Migration

--- Project/Models/Projects.cs
+++ Project/Models/Projects.cs
@@ -8,7 +8,7 @@ namespace Project.Models
         public string Name { get; set; }//название проекта
         public string Customer { get; set; }//название компании-заказчика
         public string Executor { get; set; }//название компании-исполнителя
-        public string Manager { get; set; }//руководитель проекта
+        public int Manager { get; set; }//руководитель проекта
         public DateTime Date_start { get; set; }//дата начала проекта
         public DateTime Date_end { get; set; }//дата окончания проекта
         public string Info_Executor { get; set; }//данные об исполнителях проекта
---
+++ Project/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }//название задачи,
+        public int Author { get; set; }//автор задачи
+        public int Executor { get; set; }//исполнитель задачи
+        public string Status { get; set; }//статус задачи
+        public string Comment { get; set; }//комментарий
+        public int Priority { get; set; }//приоритет задачи
+    }
+}
