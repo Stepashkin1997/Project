5bc3e30146759ed4def06f2691fddd3059d78c71
Никита Степашкин <33stepashkin33@gmail.com>
2019-06-02 15:54:00 +0700

    add delete and add (UpdateController)

--- Project/Controllers/UpdateController.cs
+++ Project/Controllers/UpdateController.cs
@@ -1,5 +1,6 @@
 ﻿using Project.Models;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -11,7 +12,7 @@ namespace Project.Controllers
         // GET: Update
         public ActionResult Index()
         {
-            ViewBag.List = new List<string>() { "Employees", "Projects", "Projects_Employees" };//список имен таблиц
+            ViewBag.List = new List<string>() { "Employees", "Projects", "Projects_Employees", "Task" };//список имен таблиц
             return View();
         }
 
@@ -23,18 +24,134 @@ namespace Project.Controllers
                 switch (table)//выбор таблицы по запросу
                 {
                     case "Projects":
-                        var otdel = context.Project.ToList();
+                        var otdel = context.Project.ToList().Select(a => new { a.Id, a.Name, a.Customer, a.Executor, a.Manager, Date_start=a.Date_start.ToShortDateString(), Date_end=a.Date_end.ToShortDateString(), a.Info_Executor, a.Priority }).OrderBy(a => a.Id);
                         return Json(otdel);
                     case "Projects_Employees":
                         var product = context.Project_Employee.ToList();
                         return Json(product);
                     case "Employees":
-                        var employee = context.Employee.ToList();
+                        var employee = context.Employee.ToList().Select(a => new { a.Id, a.Name, a.Surname, a.Middle_name, a.email }).OrderBy(a => a.Id);
                         return Json(employee);
+                    case "Task":
+                        var task = context.Tasks.ToList().Select(a => new { a.Id, a.Name, a.Status, a.Comment, a.Priority, a.AuthorId, a.ExecutorId }).OrderBy(a => a.Id);
+                        return Json(task);
                     default:
                         return null;
                 }
             }
+        }
+
+
+
+        //методы изменения
+        [HttpPost]
+        public ActionResult EditEmployees(Employees employees)
+        {
+            using (context = new DataContext())
+            {
+                context.Entry(employees).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult EditProjects(Projects projects)
+        {
+            using (context = new DataContext())
+            {
+                context.Entry(projects).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult EditProjects_Employees(Projects_Employees p_e)
+        {
+            using (context = new DataContext())
+            {
+                context.Entry(p_e).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult EditTask(Task task)
+        {
+            using (context = new DataContext())
+            {
+                context.Entry(task).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Select");
+        }
+
+
+        //метод удаления
+        [HttpPost]
+        public void Delete(string command)
+        {
+            using (context = new DataContext())
+            {
+                if (command != null)
+                {
+                    context.Database.ExecuteSqlCommand(@command);
+                }
+            }
+        }
+
+
+        //методы добавления
+        [HttpPost]
+        public ActionResult AddEmployees(Employees employees)
+        {
+            using (context = new DataContext())
+            {
+                context.Employee.Add(employees);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddProjects(Projects projects)
+        {
+            using (context = new DataContext())
+            {
+                try
+                {
+                    context.Project.Add(projects);
+                    context.SaveChanges();
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+           
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddProjects_Employees(Projects_Employees p_e)
+        {
+            using (context = new DataContext())
+            {
+                context.Project_Employee.Add(p_e);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddTask(Task task)
+        {
+            using (context = new DataContext())
+            {
+                context.Tasks.Add(task);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Project/Scripts/Script.js
+++ Project/Scripts/Script.js
@@ -1,4 +1,7 @@
-﻿var commands = new Map();
+﻿var wrap;
+
+
+var commands = new Map();
 var length;
 var table;
 var lmap = commands.size;
@@ -19,125 +22,80 @@ $(document).ready(function () {
     });
 
     $('#table').on("click", ".delete", function () {
-        if ($(this).parent().parent().attr('class') == 'add') {
-            $(this).parent().parent().remove();
-        }
-        else {
-            $(this).attr('map', commands.size);
-            commands.set(lmap++, "DELETE FROM " + $("select#update").val() + " WHERE id=" + $(this).parent().parent().attr('id') + "; ")
-            $(this).parent().parent().children("td").css('background-color', 'blue');
-            $(this).attr('src', '/Content/img/plus.png');
-            $(this).attr('class', 'recover');
-        }
-    });
-
-    $('#table').on("click", ".recover", function () {
-        $(this).parent().parent().children("td").css("background-color", "");
-        $(this).attr('src', '/Content/img/minus.png');
-        $(this).attr('class', 'delete');
-        commands.delete(Number($(this).attr('map')));
-    });
-
-    $('#confirm').bind('click', function () {
-        var b = false;
-        $('#table').children('.add').each(function () {
-            var params = "";
-            var values = "";
-            var i = 0;
-            $(this).children('td').each(function () {
-                i++;
-                if (i == lrow) {
-                    params += $(this).attr('id');
-                    values += "'" + $(this).children('input').val() + "'";
-                    if ($(this).children('input').val() == null) {
-                        alert("Заполните ячейки полностью!");
-                        b = true;
-                        return;
-                    }
+        if (confirm("Are you sure?")) {
+            var del = "DELETE FROM " + $("select#update").val() + " WHERE id=" + $(this).parent().parent().attr('id') + "; ";
+            $.ajax({
+                url: "/Update/Delete/",
+                type: "POST",
+                dataType: "text",
+                data: "command=" + del,
+                success: function () {
+                    $(this).parent().parent().remove();
+                    alert("Delete");
+                },
+                error: function () {
+                    alert("Server is fallen");
                 }
-                else {
-                    params += $(this).attr('id') + ",";
-                    values += "'" + $(this).children('input').val() + "',";
-                }
-            })
-            commands.set(lmap++, "INSERT INTO " + $("select#update").val() + "(" + params + ")" + " VALUES (" + values + "); ");
-        });
-        if (b)
-            return;
-        var com;
-        commands.forEach(function (value) {
-            com += value;
-        });
-        $.ajax({
-            url: "/Update/Change/",
-            type: "POST",
-            data: {
-                "commands": com
-            },
-            error: function (response) {
-                alert("Server is fallen");
-            }
-        });
+            });
+        } else;
     });
 
-    $('#add').bind('click', function () {
-        $("#table").append("<tr class='add' id='" + length + "'>");
-        for (var key in table[0]) {
-            if (key == "id") {
-                continue;
-            }
-            else {
-                $("#" + length + "").append("<td id='" + key + "'></td>");
-            }
-        }
-        $("#" + length + "").append("<th><img src='/Content/img/minus.png' class='delete'/></th>");
-        length++;
-        $(".delete").css('cursor', ' pointer');
-    });
 
     $('#table').on("click", "td", function (e) {
 
-        var t = e.target || e.srcElement;
-        var val = $(this).text();
-        $(this).html("<input type='text' value='" + val + "' name='" + $(this).attr('id') + "' id='" + $(this).attr('id') + "'/>");
-        $(this).find('input').focus();
-        $(this).find('input').blur(function (e) {
-            if ($(this).parent().parent().attr('class') == 'add')
-                return;
-            if (val != $(this).val()) {
-                var a = $(this).parent().parent().attr('id');
-                $(this).attr('map', commands.size);
-                commands.set(lmap++, "UPDATE " + $("select#update").val() + " SET " + $(this).attr('id') + "='" + $(this).val() + "' WHERE id=" + $(this).parent().parent().attr('id') + "; ")
-                $(this).attr('readonly', true).removeAttr("id");
-            }
-            else {
-                $(this).parent().html($(this).val());
-            }
-        });
+        //var t = e.target || e.srcElement;
+        //var val = $(this).text();
+        //$(this).html("<input type='text' value='" + val + "' name='" + $(this).attr('id') + "' id='" + $(this).attr('id') + "'/>");
+        //$(this).find('input').focus();
+        //$(this).find('input').blur(function (e) {
+        //    if ($(this).parent().parent().attr('class') == 'add')
+        //        return;
+        //    if (val != $(this).val()) {
+        //        var a = $(this).parent().parent().attr('id');
+        //        $(this).attr('map', commands.size);
+        //        commands.set(lmap++, "UPDATE " + $("select#update").val() + " SET " + $(this).attr('id') + "='" + $(this).val() + "' WHERE id=" + $(this).parent().parent().attr('id') + "; ")
+        //        $(this).attr('readonly', true).removeAttr("id");
+        //    }
+        //    else {
+        //        $(this).parent().html($(this).val());
+        //    }
+        //});
     });
 });
 
 function onAjaxSuccess(data) {
+    //первоначальная отчистка
     $("#add").empty();
     $("#table").empty();
+    $("#AddForm").empty();
+
+    //parse
     data = jQuery.parseJSON(data);
     table = data;
+
+
     $("#table").append("<caption><h1>" + $("select option:selected").html() + "</h1></caption>");
+    $("#AddForm").attr('action', "Add" + $("select option:selected").html());
+
     $("#table").append("<tr id='title'>");
     for (var key in data[0]) {
-        if (key == "id") {
+        if (key == "Id") {
             continue;
         }
         lrow++;
         $("#title").append("<th>" + key + "</th>");
+        $("#AddForm").append("<h3>" + key + "</h3>");
+        $("#AddForm").append("<input type='text' name='" + key + "'>");
     }
+    $("#AddForm").append("<input type='submit'/>");
     $("#title").append("<th>delete</th>");
+
     length = data.length + 1;
     for (var i = 0; i < data.length; i++) {
         $("#table").append("<tr id='" + data[i].Id + "'>");
 
         for (var key in data[i]) {
-            if (key == "id") {
+            if (key == "Id") {
                 continue;
             }
             else {
@@ -149,5 +107,23 @@ function onAjaxSuccess(data) {
     $("#add").append("<img src='/Content/img/plus.png' id='plus'>");
     $("#add").css('cursor', ' pointer');
     $(".delete").css('cursor', ' pointer');
-}
 
+    wrap = $('#wrapper'),
+        btn = $('#add'),
+        modal = $('.cover, .modal, .content');
+
+    btn.on('click', function () {
+        modal.fadeIn();
+    });
+
+    // close modal
+    $('.modal').click(function () {
+        wrap.on('click', function (event) {
+            var select = $('.content');
+            if ($(event.target).closest(select).length)
+                return;
+            modal.fadeOut();
+            wrap.unbind('click');
+        });
+    });
+}
